d331c404488fe5d38489d4be3face65b3c7fb11d
Will Shi <drinkthere@gmail.com>
2024-04-16 22:43:49 +0800

    add stat.js

---
+++ stat.js
@@ -0,0 +1,197 @@
+const AsyncLock = require("async-lock");
+const { scheduleLoopTask, sleep, fileExists } = require("./utils/run");
+const { log } = require("./utils/log");
+const OkxClient = require("./clients/okx");
+const StatOrderService = require("./services/statOrder");
+
+const cfgFile = `./configs/config.json`;
+if (!fileExists(cfgFile)) {
+    log(`config file ${cfgFile} does not exits`);
+    process.exit();
+}
+const configs = require(cfgFile);
+
+const { account } = require("minimist")(process.argv.slice(2));
+if (account == null) {
+    log("node stat.js --account=xxx");
+    process.exit();
+}
+
+const keyIndex = configs.keyIndexMap[account];
+// 加载.env文件
+const dotenv = require("dotenv");
+dotenv.config();
+const apiKeyArr = process.env.OKX_STAT_API_KEY.split(",");
+const apiSecretArr = process.env.OKX_STAT_API_SECRET.split(",");
+const apiPasswordArr = process.env.OKX_STAT_API_PASSWORD.split(",");
+
+let options = {
+    API_KEY: apiKeyArr[keyIndex],
+    API_SECRET: apiSecretArr[keyIndex],
+    API_PASSWORD: apiPasswordArr[keyIndex],
+};
+const exchangeClient = new OkxClient(options);
+
+// 初始化同步锁
+const lock = new AsyncLock();
+
+// 初始化stat order service
+const statOrderService = new StatOrderService();
+
+let volContractMap = {};
+const loadVolContractInfo = async () => {
+    let symbols = await exchangeClient.getFuturesSymbols();
+    symbols = symbols.filter(
+        (symbolInfo) => symbolInfo.symbol.indexOf("USDC") == -1
+    );
+    for (let symbolInfo of symbols) {
+        volContractMap[symbolInfo.symbol] = parseFloat(symbolInfo.ctVal);
+    }
+};
+
+const orderUpdateHandler = async (orders) => {
+    for (let order of orders) {
+        // 使用clientOrderId作为锁的key，避免并发引起的更新错误
+        const clientOrderId = order.clientOrderId;
+        await lock.acquire(clientOrderId, async () => {
+            if (["FILLED"].includes(order.orderStatus)) {
+                const symbol = order.symbol;
+                if (!volContractMap.hasOwnProperty(symbol)) {
+                    console.error(`${symbol}'s ctVal configuration is missing`);
+                    return;
+                }
+
+                const side = order.side;
+                const quantity = order.originalQuantity;
+                const amount = quantity * parseFloat(volContractMap[symbol]);
+                const price = order.lastFilledPrice;
+                const notional = price * amount;
+
+                const msg = `${account} ${symbol} ${side} ${order.orderStatus} ${amount}@${price}`;
+                log(msg);
+
+                // 将订单写入数据库
+                await statOrderService.saveOrder(`tb_order_${account}`, {
+                    symbol,
+                    side,
+                    quantity,
+                    amount,
+                    price,
+                    notional,
+                });
+            }
+        });
+    }
+};
+
+const scheduleStatProfit = () => {
+    scheduleLoopTask(async () => {
+        try {
+            const tickersMap = await exchangeClient.getFuturesTickers();
+
+            const balances = await exchangeClient.getFuturesBalances();
+            let usdtBalanceArr = balances
+                .filter((item) => item.asset == "USDT")
+                .map((item) => item.balance);
+            const usdtBalance =
+                usdtBalanceArr.length == 0 ? 0 : usdtBalanceArr[0];
+
+            let tradingBalance = balances.reduce((total, item) => {
+                return total + parseFloat(item.notional);
+            }, 0);
+
+            const fundingBalanceArr =
+                await exchangeClient.getFundingAccountBalances();
+            let fundingBalance = fundingBalanceArr.reduce((total, item) => {
+                return (
+                    total +
+                    parseFloat(item.balance) *
+                        parseFloat(
+                            tickersMap[item.asset + "-USDT-SWAP"]["bestBid"]
+                        )
+                );
+            }, 0);
+            log(
+                `tradingBalance=${tradingBalance}, fundingBalance=${fundingBalance}`
+            );
+
+            const positions = await exchangeClient.getPositions();
+            let notionalBTCETH = 0;
+            let notionalOther = 0;
+            let positionsNum = 0;
+            if (positions != null) {
+                for (let position of positions) {
+                    positionsNum++;
+                    if (
+                        ["ETH-USDT-SWAP", "BTC-USDT-SWAP"].includes(
+                            position.symbol
+                        )
+                    ) {
+                        notionalBTCETH += position.notional;
+                    } else {
+                        notionalOther += position.notional;
+                    }
+                }
+            }
+            const notionalAll = notionalBTCETH + notionalOther;
+            let msg = `${account} USDTBalance=${usdtBalance.toFixed(
+                2
+            )}|PositionDeltaWithBTCETH=${notionalBTCETH.toFixed(
+                2
+            )}|PositionDeltaWithoutBTCETH$=${notionalOther.toFixed(
+                2
+            )}|PositionDeltaAll=${notionalAll.toFixed(
+                2
+            )}|PositionCount=${positionsNum}`;
+            log(msg);
+
+            // 获取margin ratio
+            let marginRatio = await exchangeClient.getMarginRatio();
+            marginRatio = marginRatio ? marginRatio : 0;
+
+            // 获取openorders数量
+            let buyOrdersNum = 0;
+            let sellOrdersNum = 0;
+            const orders = await exchangeClient.getFuturesOpenOrderList();
+            for (let order of orders) {
+                if (order.side == "BUY") {
+                    buyOrdersNum++;
+                } else {
+                    sellOrdersNum++;
+                }
+            }
+            const ordersNum = buyOrdersNum + sellOrdersNum;
+            console.log(
+                `The num of open orders is ${ordersNum}(B:${buyOrdersNum}|S:${sellOrdersNum})`
+            );
+
+            // 将订单写入数据库
+            await statOrderService.saveBalance({
+                account,
+                usdt_balance: usdtBalance.toFixed(2),
+                trading_balance: tradingBalance.toFixed(2),
+                funding_balance: fundingBalance.toFixed(2),
+                btc_eth_delta: notionalBTCETH.toFixed(2),
+                other_delta: notionalOther.toFixed(2),
+                total_delta: notionalAll.toFixed(2),
+                margin_ratio: marginRatio.toFixed(2),
+                orders_num: ordersNum,
+                position_count: positionsNum,
+            });
+        } catch (e) {
+            console.error(e);
+        }
+        await sleep(120 * 1000);
+    });
+};
+
+const main = async () => {
+    await loadVolContractInfo();
+    exchangeClient.initWsEventHandler({
+        orders: orderUpdateHandler,
+    });
+    exchangeClient.wsFuturesOrders();
+
+    scheduleStatProfit();
+};
+main();
